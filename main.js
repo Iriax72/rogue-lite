537759e24e35f861570055e73a12569fce9e73c3
Léandre <touma.leandre.72@gmail.com>
2026-09-04 15:35:39 +0000

    patch

--- main.js
+++ main.js
@@ -2,10 +2,11 @@
 import {Game} from './class/Game.js';
 
 // References DOM
-const gameCanvas = document.quetySelector('#game-canvas');
+const gameCanvas = document.querySelector('#game-canvas');
 
 // Initialisation
 let map = await fetch('./map.json');
+map = await map.json();
 map = JSON.parse(map);
 const game = new Game(gameCanvas, map[0]);
 game.init();
