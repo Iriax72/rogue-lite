e1df7db07e969b592951a6fb6e1912bca899b56c
Léandre <touma.leandre.72@gmail.com>
2026-09-04 15:46:00 +0000

    patch logique

--- class/Game.js
+++ class/Game.js
@@ -18,9 +18,10 @@ export class Game {
         const ctx = this.canvas.getContext('2d');
         ctx.clearRect(0, 0, this.canvas.width, this.canvas.height);
 
-        for (let x = 0; x < this.map.length; x++) {
-            for (let y = 0; y < this.map[0].length; y++) {
-                ctx.fillStyle = this.map[x][y] ? "#000" : "#777";
+        // Dessiner la carte
+        for (let y = 0; y < this.map.length; y++) {
+            for (let x = 0; x < this.map[y].length; x++) {
+                ctx.fillStyle = this.map[y][x] ? "#000" : "#777";
                 ctx.fillRect(x * this.TILE_SIZE, y * this.TILE_SIZE, this.TILE_SIZE, this.TILE_SIZE);
             }
         }
