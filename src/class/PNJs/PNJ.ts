e44fb12f4d57a07acaaf1073bf333c807e470ee1
Léandre <touma.leandre.72@gmail.com>
2026-09-23 17:26:16 +0200

    encore mieux comprendre

--- src/class/PNJs/PNJ.ts
+++ src/class/PNJs/PNJ.ts
@@ -20,6 +20,7 @@ export abstract class PNJ extends Entity {
 
     public update(): void {
         const mousePos = this.inputs.getMousePos();
+        console.log(dist(this.getRect(), this.game.player.getRect()), this.inputs.mouse.justDown, collides(this.getRect(), {x: mousePos.x, y: mousePos.y, w: 0, h: 0}));
         if (
             dist(this.getRect(), this.game.player.getRect()) <= this.game.MAX_DIALOG_DIST
             && this.inputs.mouse.justDown
